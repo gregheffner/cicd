4c2b639055543feef0d49f8bc8ae91824164e4dc
gregheffner <gregheffner@comcast.net>
2026-01-11 20:01:32 -0500

    updated docker-generate

--- .github/scripts/generate_dockerfile.py
+++ .github/scripts/generate_dockerfile.py
@@ -71,6 +71,7 @@ NJS_RELEASE, PKG_RELEASE = get_latest_njs_debian_release()
 base_image = f"nginx:{NGINX_VERSION}-alpine-slim"
 
 dockerfile_content = f"""\
+
 FROM {base_image}
 
 LABEL description="Built by technotuba for K8s NGINX WWW"
@@ -78,24 +79,20 @@ LABEL maintainer="main.plan5783@fastmail.com"
 LABEL ClusterAge="{datetime.datetime.now().strftime('%a %b %d %I:%M:%S %p %Z %Y')}"
 LABEL source="https://github.com/gregheffner/k8-nginx-webpage"
 
-ENV NGINX_VERSION={NGINX_VERSION} \\
-    NJS_VERSION={NJS_VERSION} \\
-    NJS_RELEASE={NJS_RELEASE} \\
+ENV NGINX_VERSION={NGINX_VERSION} \
+    NJS_VERSION={NJS_VERSION} \
+    NJS_RELEASE={NJS_RELEASE} \
     PKG_RELEASE={PKG_RELEASE}
-
-RUN set -x && \\
-    rm -rf /var/lib/apt/lists/*
-
-RUN apk add --no-cache tzdata && \
-    cp /usr/share/zoneinfo/America/New_York /etc/localtime && \
-    echo "America/New_York" > /etc/timezone
 ENV TZ=America/New_York
+
+RUN set -x && apk add --no-cache tzdata && \
+    cp /usr/share/zoneinfo/America/New_York /etc/localtime && \
+    echo "America/New_York" > /etc/timezone && \
+    chmod +x /docker-entrypoint.sh && \
+    chmod +x /docker-entrypoint.d/*.sh || true
 
 COPY DockerImage/docker-entrypoint.sh /
 COPY DockerImage/docker-entrypoint.d/ /docker-entrypoint.d/
-
-RUN chmod +x /docker-entrypoint.sh && \\
-    chmod +x /docker-entrypoint.d/*.sh || true
 
 EXPOSE 80
 
